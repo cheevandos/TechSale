13319697840b1a6fd2c8af5c025b9ecbc6edb638
LyaysanIsm <53337361+LyaysanIsm@users.noreply.github.com>
2020-12-12 18:22:21 +0400

    add personal account

--- TechSale/WebApplicationTechSale/Controllers/AccountController.cs
+++ TechSale/WebApplicationTechSale/Controllers/AccountController.cs
@@ -19,6 +19,70 @@ namespace WebApplicationTechSale.Controllers
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword()
+        {
+            User user = await userManager.FindByIdAsync(userManager.FindByNameAsync(User.Identity.Name).Result.Id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ChangePasswordViewModel model = new ChangePasswordViewModel
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var _passwordValidator =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+                    var _passwordHasher =
+                        HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+
+                    IdentityResult result =
+                        await _passwordValidator.ValidateAsync(userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                        await userManager.UpdateAsync(user);
+                        return RedirectToAction("Personal");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь не найден");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Personal()
+        {
+            var user = userManager.FindByNameAsync(User.Identity.Name);
+            PersonalAccountViewModel model = new PersonalAccountViewModel
+            {
+                Email = user.Result.Email,
+                TelegramId = user.Result.TelegramUsername
+            };
+            return View(model);
+        }
 
         [HttpGet]
         public IActionResult Login(string returnUrl)
@@ -84,7 +148,7 @@ namespace WebApplicationTechSale.Controllers
                 {
                     Email = model.Email,
                     UserName = model.Email,
-                    TelegramUsername = string.IsNullOrWhiteSpace(model.TelegramId) ? 
+                    TelegramUsername = string.IsNullOrWhiteSpace(model.TelegramId) ?
                     string.Empty : model.TelegramId
                 };
                 var registerResult = await userManager.CreateAsync(user, model.Password);
---
+++ TechSale/WebApplicationTechSale/Models/PersonalAccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTechSale.Models
+{
+    public class PersonalAccountViewModel
+    {
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+
+        [Display(Name = "Имя пользователя в Telegram")]
+        public string TelegramId { get; set; }
+
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
